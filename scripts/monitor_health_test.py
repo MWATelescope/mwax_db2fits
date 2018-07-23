3c776d308dc074ac558c2d122383a463c199cf4e
Greg Sleap <greg.sleap@curtin.edu.au>
2018-07-23 16:40:10 +0800

    cleaned up monitor health code

--- scripts/monitor_health_test.py
+++ scripts/monitor_health_test.py
@@ -15,6 +15,8 @@ try:
     # Receive response
     print("listening on port {0}...".format(port))
 
+    MEGABYTE = 1000*1000
+
     while True:
         data, server = sock.recvfrom(4096)
 
@@ -24,13 +26,13 @@ try:
          uint64_hdr_clear_bufs, uint64_hdr_available_bufs, uint64_data_bufs_written, uint64_data_bufs_read,
          uint64_data_full_bufs, uint64_data_clear_bufs, uint64_data_available_bufs) = struct.unpack(fmt, data)
 
-        print("{0}: Status: {1} Readers: {2} Buffs tot: {3} {4}MB avail: {5} {6}MB clear: {7} full: {8} read: {9} {10} MB".format(
-            datetime.datetime.now().strftime("%Y-%m-%d %H:%M"), sint32_status, sint32_n_readers,
-            sint32_data_nbufs, (uint64_data_bytes / (1000*1000)),
-            uint64_data_available_bufs, (uint64_data_available_bufs*uint64_data_bufsz/(1000*1000)),
+        print("{0}: Status: {1} Readers: {2} Buffs tot: {3} {4:.1f}MB avail: {5} {6:.1f}MB clear: {7} full: {8} read: {9} {10:.1f} MB".format(
+            datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), sint32_status, sint32_n_readers,
+            sint32_data_nbufs, (uint64_data_bytes / MEGABYTE),
+            uint64_data_available_bufs, (uint64_data_available_bufs*uint64_data_bufsz/MEGABYTE),
             uint64_data_clear_bufs,
             uint64_data_full_bufs,
-            uint64_data_bufs_read, (uint64_data_bufs_read*uint64_data_bufsz/(1000*1000))))
+            uint64_data_bufs_read, (uint64_data_bufs_read*uint64_data_bufsz/MEGABYTE)))
         sys.stdout.flush()
 finally:
     print("closing socket")
