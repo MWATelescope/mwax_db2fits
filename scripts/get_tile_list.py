cfccf5f466131986273b03b0f7065a80cb04281a
Greg Sleap <greg.sleap@curtin.edu.au>
2019-09-06 17:37:08 +0800

    Added quick and dirty tile order listing utility which reads from metafits and provides the order of tiles that would exist in the mwax sub and fits data.

---
+++ scripts/get_tile_list.py
@@ -0,0 +1,41 @@
+from astropy.io import fits
+import argparse
+
+
+def dump(filename):
+    fits_hdu_list = fits.open(filename)
+
+    # Determine the tile order
+    rf_list = []
+    tile_list = []
+
+    for row in fits_hdu_list[1].data:
+        rf_order = -1
+        tile_order = -1
+
+        if row["Pol"] == "X":
+            rf_order = row["Antenna"] * 2
+            tile_list.append((row["Antenna"], row["Tile"], row["TileName"]))
+
+        elif row["Pol"] == "Y":
+            rf_order = (row["Antenna"] * 2) + 1
+
+        rf_list.append((rf_order, row["Antenna"], row["Pol"], row["Tile"], row["TileName"]))
+
+    rf_list.sort()
+    tile_list.sort()
+
+    print("Order, Tile, TileName")
+    for tile in tile_list:
+        print(tile)
+
+    # clean up
+    fits_hdu_list.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="metafits filename")
+    args = vars(parser.parse_args())
+
+    dump(args["filename"])
